65ba8804d530bc429a236418776a31618153ada9
raskinminsk <raskin.minsk@gmail.com>
2024-01-02 19:44:49 +0300

    1 from Ubuntu 2204

---
+++ Ubuntu2204_openvpn/setupvpn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "deb http://repo.pritunl.com/stable/apt focal main" | sudo tee /etc/apt/sources.list.d/pritunl.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+curl https://raw.githubusercontent.com/pritunl/pgp/master/pritunl_repo_pub.asc | sudo apt-key add -
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+apt update
+apt -y install wireguard wireguard-tools
+ufw disable
+apt -y install pritunl mongodb-org
+systemctl enable mongod pritunl
+systemctl start mongod pritunl
